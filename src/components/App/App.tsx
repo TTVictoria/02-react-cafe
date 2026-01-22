78b12206f008364cb6fc27b8f2a014aafd5fbbca
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2026-01-22 20:52:56 +0100

    final main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,60 +1,37 @@
 import { useState } from "react";
 import css from "./App.module.css";
-
-// Импортируем компоненты
+import type { Votes, VoteType } from "../../types/votes";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
-import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
-
-// Импортируем типы
-import { Votes, VoteType } from "../../types/votes";
+import VoteStats from "../VoteStats/VoteStats";
 
 export default function App() {
-  // Состояние голосов
-  const [votes, setVotes] = useState<Votes>({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
-  // Функция для голосования
   const handleVote = (type: VoteType) => {
-    setVotes((prev) => ({
-      ...prev,
-      [type]: prev[type] + 1,
-    }));
+    setVotes({ ...votes, [type]: votes[type] + 1 });
   };
-
-  // Функция для сброса голосов
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
-  // Вычисляем общие показатели
-  const totalVotes = votes.good + votes.neutral + votes.bad;
-  const positiveRate = totalVotes
-    ? Math.round((votes.good / totalVotes) * 100)
-    : 0;
+  const totalVotes = Object.values(votes).reduce((i, vote) => i + vote, 0);
+  const positive = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
 
   return (
     <div className={css.app}>
-      {/* Информация о кафе */}
       <CafeInfo />
-
-      {/* Кнопки голосования */}
       <VoteOptions
         onVote={handleVote}
         onReset={resetVotes}
         canReset={totalVotes > 0}
       />
-
-      {/* Статистика или уведомление */}
       {totalVotes > 0 ? (
         <VoteStats
           votes={votes}
           totalVotes={totalVotes}
-          positiveRate={positiveRate}
+          positiveRate={positive}
         />
       ) : (
         <Notification />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Импорт главного компонента App
+import App from "./components/App/App";
+
+import "./index.css";
+
+// Рендер приложения в корневой элемент
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
